99d42172369b6221dd9e671f277fb685449aa2da
Pujit Shetty <pujit.shetty4606@gmail.com>
2026-07-11 18:50:42 +0530

    Html done

---
+++ frontend/script.js
@@ -0,0 +1,8 @@
+const promptInput = document.getElementById("prompt-input");
+const filenameInput = document.getElementById("filename-input");
+const generateButton = document.getElementById("generate-btn");
+const statusText = document.getElementById("status-text");
+const videoPlayer = document.getElementById("video_player");
+
+console.log(promptInput);
+console.log(generateButton);
